72f674bcf7d6cf0c40b7a1ab5ab09e4dccb0db98
zhutao <zhutao@qudian.com>
2018-12-09 16:39:24 +0800

    方法句柄

---
+++ src/main/java/com/lfp/zt/javabase/methodHandle/Client.java
@@ -0,0 +1,39 @@
+package com.lfp.zt.javabase.methodHandle;
+
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+
+/**
+ * Project: zt-javabase
+ * Title:
+ * Description:
+ * Date: 2018-12-09
+ * Copyright: Copyright (c) 2018
+ * Company: qudian
+ *
+ * @author ZhuTao
+ * @version 2.0
+ */
+public class Client {
+
+
+    private void invokeExact() throws Throwable {
+        MethodType type = MethodType.methodType(String.class, String.class);
+
+        MethodHandle mh = MethodHandles.lookup().findVirtual(Demo.class, "test", type);
+
+        String ret = (String)mh.invokeExact(new Demo(), "Hello World!");
+
+        System.out.println(ret);
+    }
+
+    public static void main(String[] args){
+        Client client = new Client();
+        try {
+            client.invokeExact();
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/lfp/zt/javabase/methodHandle/Demo.java
@@ -0,0 +1,20 @@
+package com.lfp.zt.javabase.methodHandle;
+
+/**
+ * Project: zt-javabase
+ * Title:
+ * Description:
+ * Date: 2018-12-09
+ * Copyright: Copyright (c) 2018
+ * Company: qudian
+ *
+ * @author ZhuTao
+ * @version 2.0
+ */
+public class Demo {
+
+    public String test(String param){
+        return param;
+    }
+
+}
