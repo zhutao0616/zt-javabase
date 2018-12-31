0a193255f20a7b795d1a6b1d949563b1a7bfe65c
zhutao <zhutao@qudian.com>
2018-12-31 17:20:47 +0800

    添加六种常用排序方法。冒泡/选择/插入/归并/快排/堆排

--- src/main/java/com/lfp/zt/algorithm/sort/impl/HeapSort.java
+++ src/main/java/com/lfp/zt/algorithm/sort/impl/HeapSort.java
@@ -6,6 +6,8 @@ import com.lfp.zt.algorithm.sort.Sort;
  * Project: zt-javabase
  * Title: 堆排序
  * Description: 构建一个大顶堆，然后第一个就是最大值，替换到最后，继续将剩下的构建大顶堆，继续直到最后
+ *              递归调用堆调整方法实现，堆调整就是将一个三角子堆，调整成最大值放到最上面，然后动了那个子节点，子节点的三角子堆也要跟着调整。
+ *              构建大顶堆时，是自下而上调整子堆即可。注意控制堆的长度。
  * Date: 2018-12-31
  * Copyright: Copyright (c) 2018
  * Company: qudian
